a0a23eb9454411a6ed90c1d0760441bc09fb70d8
DeepakTayde <deepaktayde559@gmail.com>
2025-06-30 18:30:28 +0530

    implemented initial struscture and implemented material ui theme

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -1,9 +1,123 @@
-import React from 'react'
+import { Box, Grid, Stack, Typography } from "@mui/material";
+import InitialChat from "../../components/InitialChat/InitialChat";
+import ChatInput from "../../components/ChatInput/ChatInput";
+import ChattingCard from "../../components/ChattingCard/ChattingCard";
+import FeedbackModal from "../../components/FeedbackModal/FeedbackModal";
+import { useEffect, useRef, useState } from "react";
+import data from "../../Data/sampleData.json";
+import { useOutletContext } from "react-router-dom";
+import Navbar from "../../components/Navbar/Navbar";
+import { ThemeContext } from "../../theme/ThemeContext";
+import { useContext } from "react";
 
-const Home = () => {
+export default function Home() {
+  const [showModal, setShowModal] = useState(false);
+  const listRef = useRef(null);
+  const [chatId, setChatId] = useState(1);
+  const [selectedChatId, setSelectedChatId] = useState(null);
+  const [scrollToBottom, setScrollToBottom] = useState(false);
+  const { chat, setChat } = useOutletContext();
+  const { mode } = useContext(ThemeContext);
+
+  // GENERATING AI RESPONSE
+  const generateResponse = (input) => {
+    const response = data.find(
+      (item) => input.toLowerCase() === item.question.toLowerCase()
+    );
+
+    let answer = "Sorry, Did not understand your query!";
+
+    if (response !== undefined) {
+      answer = response.response;
+    }
+
+    setChat((prev) => [
+      ...prev,
+      {
+        type: "Human",
+        text: input,
+        time: new Date(),
+        id: chatId,
+      },
+      {
+        type: "AI",
+        text: answer,
+        time: new Date(),
+        id: chatId + 1,
+      },
+    ]);
+
+    setChatId((prev) => prev + 2);
+  };
+
+  //AUTOSCROLL TO LAST ELEMENT
+  useEffect(() => {
+    listRef.current?.lastElementChild?.scrollIntoView();
+  }, [scrollToBottom]);
+
   return (
-    <div>Home</div>
-  )
-}
+    <Stack
+      height={"100vh"}
+      justifyContent={"space-between"}
+      sx={{
+        "@media (max-width:767px)": {
+          background:
+            mode === "light" ? "linear-gradient(#F9FAFA 60%, #EDE4FF)" : "",
+        },
+      }}
+    >
+      <Navbar />
 
-export default Home
+      {chat.length === 0 && <InitialChat generateResponse={generateResponse} />}
+
+      {chat.length > 0 && (
+        <Grid
+          container
+          p={{ xs: 2, md: 3 }}
+          spacing={{ xs: 2, md: 3 }}
+          sx={{
+            overflowY: "auto",
+            height: 1,
+            "&::-webkit-scrollbar": {
+              width: "10px",
+            },
+            "&::-webkit-scrollbar-track": {
+              boxShadow: "inset 0 0 8px rgba(0,0,0,0.1)",
+              borderRadius: "8px",
+            },
+            "&::-webkit-scrollbar-thumb": {
+              backgroundColor: "rgba(151, 133, 186,0.4)",
+              borderRadius: "8px",
+            },
+          }}
+          ref={listRef}
+        >
+          {chat.map((item, index) => (
+            <Grid item xs={12} sm={6} key={index}>
+              <ChattingCard
+                details={item}
+                updateChat={setChat}
+                setSelectedChatId={setSelectedChatId}
+                showFeedbackModal={() => setShowModal(true)}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      )}
+
+      <ChatInput
+        generateResponse={generateResponse}
+        setScroll={setScrollToBottom}
+        chat={chat}
+        clearChat={() => setChat([])}
+      />
+
+      <FeedbackModal
+        open={showModal}
+        updateChat={setChat}
+        chatId={selectedChatId}
+        handleClose={() => setShowModal(false)}
+      />
+    </Stack>
+  );
+}
